6fc0dee5af7d0e1675399fc2854649e55b4083ae
ostrokach <ostrokach@gmail.com>
2019-10-04 15:17:17 -0400

    Add SudokuDataset4

--- proteinsolver/datasets/__init__.py
+++ proteinsolver/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .download import download_url
 from .sudoku import SudokuDataset
-from .sudoku_v2 import SudokuDataset2, SudokuDataset3
+from .sudoku_v2 import SudokuDataset2, SudokuDataset3, SudokuDataset4
 from .protein import *
--- proteinsolver/datasets/sudoku_v2.py
+++ proteinsolver/datasets/sudoku_v2.py
@@ -1,11 +1,125 @@
 from typing import Optional
 
 import pandas as pd
-import torch
-from torch_geometric.data import Data, Dataset, InMemoryDataset
 
+import pyarrow.parquet as pq
+import torch
 from proteinsolver.datasets import download_url
 from proteinsolver.utils import construct_solved_sudoku, gen_sudoku_graph, str_to_tensor
+from torch_geometric.data import Data, Dataset, InMemoryDataset
+
+
+class SudokuDataset4(Dataset):
+    def __init__(
+        self,
+        root,
+        subset: Optional[str] = None,
+        data_url: Optional[str] = None,
+        transform=None,
+        pre_transform=None,
+        pre_filter=None,
+    ) -> None:
+        """Create new SudokuDataset."""
+        if data_url is None:
+            assert subset is not None
+            self.data_url = (
+                "https://storage.googleapis.com/deep-protein-gen/sudoku_difficult/"
+                f"{subset.replace('sudoku_', '')}.parquet"
+            )
+        else:
+            self.data_url = data_url
+        self._raw_file_names = [self.data_url.rsplit("/")[-1]]
+        self._edge_index, _ = gen_sudoku_graph()
+        super().__init__(root, transform, pre_transform, pre_filter)
+
+        #
+        self.file = pq.ParquetFile(self.raw_paths[0])
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.prev_index = None
+        self.row_group = None
+        self.data_chunk = None
+        self.data_chunk_idx = None
+
+    @property
+    def raw_file_names(self):
+        return self._raw_file_names
+
+    @property
+    def processed_file_names(self):
+        return []
+
+    def download(self):
+        download_url(self.data_url, self.raw_dir)
+
+    def process(self):
+        pass
+
+    def __len__(self):
+        return self.file.metadata.num_rows
+
+    def _read_row_group(self, row_group):
+        df = self.file.read_row_group(row_group).to_pandas(integer_object_nulls=True)
+
+        data_list = []
+        for tup in df.itertuples():
+            puzzle = str_to_tensor(tup.puzzle) - 1
+            puzzle = torch.where(puzzle >= 0, puzzle, torch.tensor(9))
+            solution = str_to_tensor(tup.solution) - 1
+            data = Data(x=puzzle, y=solution)
+            data_list.append(data)
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        return data_list
+
+    def get(self, idx):
+        if self.prev_index is None:
+            assert idx == 0, idx
+            self.row_group = 0
+            self.data_chunk = self._read_row_group(self.row_group)
+            self.data_chunk_idx = 0
+        else:
+            assert self.prev_index == idx - 1
+            self.data_chunk_idx += 1
+        self.prev_index = idx
+
+        if self.data_chunk_idx >= len(self.data_chunk):
+            self.row_group += 1
+            self.data_chunk = self._read_row_group(self.row_group)
+            self.data_chunk_idx = 0
+
+        data = self.data_chunk[self.data_chunk_idx]
+        data.edge_index = self._edge_index
+        return data
+
+
+class SudokuDataset3(Dataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None) -> None:
+        self._gen_puzzle = construct_solved_sudoku
+        self._edge_index, _ = gen_sudoku_graph()
+        super().__init__(root, transform, pre_transform, pre_filter)
+
+    @property
+    def raw_file_names(self):
+        return []
+
+    @property
+    def processed_file_names(self):
+        return []
+
+    def __len__(self):
+        return 700_000  # To be consistent with SudokuDataset2
+
+    def get(self, idx):
+        puzzle = torch.from_numpy(self._gen_puzzle().reshape(-1) - 1)
+        data = Data(x=puzzle, edge_index=self._edge_index)
+        return data
 
 
 class SudokuDataset2(InMemoryDataset):
@@ -66,27 +180,4 @@ class SudokuDataset2(InMemoryDataset):
     def get(self, idx):
         data = super().get(idx)
         data.edge_index = self._edge_index
-        return data
-
-
-class SudokuDataset3(Dataset):
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None) -> None:
-        self._gen_puzzle = construct_solved_sudoku
-        self._edge_index, _ = gen_sudoku_graph()
-        super().__init__(root, transform, pre_transform, pre_filter)
-
-    @property
-    def raw_file_names(self):
-        return []
-
-    @property
-    def processed_file_names(self):
-        return []
-
-    def __len__(self):
-        return 700_000  # To be consistent with SudokuDataset2
-
-    def get(self, idx):
-        puzzle = torch.from_numpy(self._gen_puzzle().reshape(-1) - 1)
-        data = Data(x=puzzle, edge_index=self._edge_index)
         return data
