b0e9d91a5a51f7f5dfb282de92caff6df095ca85
ostrokach <ostrokach@gmail.com>
2019-11-22 15:14:16 -0500

    Generate html coverage report

--- .ci/conda/run_test.sh
+++ .ci/conda/run_test.sh
@@ -4,11 +4,15 @@ set -ev
 
 python -m pytest \
     -c setup.cfg \
-    --cov="${SP_DIR}/proteinsolver" \
     --color=yes \
+    --cov="${SP_DIR}/proteinsolver" \
+    --cov-report html \
     --junitxml=pytest.xml \
     tests/
+
+ls -al
 
 sed -i "s|${SP_DIR}/||g" .coverage
 mv .coverage "${RECIPE_DIR}/.coverage"
 mv pytest.xml "${RECIPE_DIR}/pytest.xml"
+mv htmlcov "${RECIPE_DIR}/htmlcov"
