600448509f92bf285c90049e9dff1292c51d8529
ostrokach <ostrokach@gmail.com>
2020-02-14 18:36:20 -0500

    Update to be compatible with the new kmtools.structure_tools.get_distances function

--- proteinsolver/utils/protein_structure.py
+++ proteinsolver/utils/protein_structure.py
@@ -11,8 +11,10 @@ class ProteinData(NamedTuple):
     distances: torch.FloatTensor
 
 
-def extract_seq_and_adj(structure, chain_id):
-    domain, result_df = get_interaction_dataset_wdistances(structure, 0, chain_id, r_cutoff=12)
+def extract_seq_and_adj(structure, chain_id, remove_hetatms=False):
+    domain, result_df = get_interaction_dataset_wdistances(
+        structure, 0, chain_id, r_cutoff=12, remove_hetatms=remove_hetatms
+    )
     domain_sequence = structure_tools.get_chain_sequence(domain)
     assert max(result_df["residue_idx_1"].values) < len(domain_sequence)
     assert max(result_df["residue_idx_2"].values) < len(domain_sequence)
@@ -29,11 +31,15 @@ def extract_seq_and_adj(structure, chain_id):
     return data
 
 
-def get_interaction_dataset_wdistances(structure, model_id, chain_id, r_cutoff=12):
+def get_interaction_dataset_wdistances(
+    structure, model_id, chain_id, r_cutoff=12, remove_hetatms=False
+):
     chain = structure[0][chain_id]
     num_residues = len(list(chain.residues))
     dd = structure_tools.DomainDef(model_id, chain_id, 1, num_residues)
-    domain = structure_tools.extract_domain(structure, [dd])
-    distances_core = structure_tools.get_distances(domain, r_cutoff, 0, groupby="residue")
+    domain = structure_tools.extract_domain(structure, [dd], remove_hetatms=remove_hetatms)
+    distances_core = structure_tools.get_distances(
+        domain.to_dataframe(), r_cutoff, groupby="residue"
+    )
     assert (distances_core["residue_idx_1"] <= distances_core["residue_idx_2"]).all()
     return domain, distances_core
