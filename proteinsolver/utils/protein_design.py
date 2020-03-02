cbc3439dcba381b0c014a8c3e813410799b6ff3f
ostrokach <ostrokach@gmail.com>
2020-03-02 15:54:38 -0500

    Reformat protein design functions

--- proteinsolver/utils/protein_design.py
+++ proteinsolver/utils/protein_design.py
@@ -1,14 +1,51 @@
 import heapq
 from dataclasses import dataclass, field
-from typing import Any, Tuple
+from typing import Any, Optional, Tuple
 
 import torch
 import torch.nn as nn
 from torch_geometric.data import Data
 
 
-@torch.no_grad()
 def get_node_proba(net, x, edge_index, edge_attr, num_categories=20):
+    raise Exception("Use get_node_outputs instead!")
+
+
+def get_node_value(net, x, edge_index, edge_attr, num_categories=20):
+    raise Exception("Use get_node_outputs instead!")
+
+
+@torch.no_grad()
+def get_node_outputs(
+    net: nn.Module,
+    x: torch.Tensor,
+    edge_index: torch.Tensor,
+    edge_attr: torch.Tensor,
+    num_categories: int = 20,
+    output_transform: Optional[str] = None,
+    oneshot: bool = False,
+) -> torch.Tensor:
+    """Return network output for each node in the reference sequence.
+
+    Args:
+        net: The network to use for making predictions.
+        x: Node attributes for the target sequence.
+        edge_index: Edge indices of the target sequence.
+        edge_attr: Edge attributes of the target sequence.
+        num_categories: The number of categories to which the network assigns individual nodes
+            (e.g. the number of amino acids for the protein design problem).
+        output_transform: Transformation to apply to network outputs.
+            - `None` - No transformation.
+            - `proba` - Apply the softmax transformation.
+            - `logproba` - Apply the softmax transformation and log the results.
+        oneshot: Whether predictions should be made using a single pass through the network,
+            or incrementally, by making a single prediction at a time.
+
+    Returns:
+        A tensor of network predictions for each node in `x`.
+    """
+    assert output_transform in [None, "proba", "logproba"]
+
     x_ref = x
     x = torch.ones_like(x_ref) * num_categories
     x_proba = torch.zeros_like(x_ref).to(torch.float)
@@ -16,32 +53,15 @@ def get_node_proba(net, x, edge_index, edge_attr, num_categories=20):
     mask = x == num_categories
     while mask.any():
         output = net(x, edge_index, edge_attr)
-        output = torch.softmax(output, dim=1)
+        if output_transform == "proba":
+            output = torch.softmax(output, dim=1)
+        elif output_transform == "logproba":
+            output = torch.softmax(output, dim=1).log()
+
         output_for_x = output.gather(1, x_ref.view(-1, 1))
 
-        output_for_x = output_for_x[mask]
-        index_array = index_array_ref[mask]
-        max_proba, max_proba_position = output_for_x.max(dim=0)
-
-        assert x[index_array[max_proba_position]] == num_categories
-        assert x_proba[index_array[max_proba_position]] == 0
-        x[index_array[max_proba_position]] = x_ref[index_array[max_proba_position]]
-        x_proba[index_array[max_proba_position]] = max_proba
-        mask = x == num_categories
-    return x_proba
-
-
-@torch.no_grad()
-def get_node_value(net, x, edge_index, edge_attr, num_categories=20):
-    x_ref = x
-    x = torch.ones_like(x_ref) * num_categories
-    x_proba = torch.zeros_like(x_ref).to(torch.float)
-    index_array_ref = torch.arange(x_ref.size(0))
-    mask = x == num_categories
-    while mask.any():
-        output = net(x, edge_index, edge_attr)
-        output_proba = torch.softmax(output, dim=1)
-        output_for_x = output_proba.gather(1, x_ref.view(-1, 1))
+        if oneshot:
+            return output_for_x.data.cpu()
 
         output_for_x = output_for_x[mask]
         index_array = index_array_ref[mask]
@@ -51,11 +71,37 @@ def get_node_value(net, x, edge_index, edge_attr, num_categories=20):
         assert x_proba[index_array[max_proba_position]] == 0
         correct_amino_acid = x_ref[index_array[max_proba_position]].item()
         x[index_array[max_proba_position]] = correct_amino_acid
-        x_proba[index_array[max_proba_position]] = output[
-            index_array[max_proba_position], correct_amino_acid
-        ]
+        assert output[index_array[max_proba_position], correct_amino_acid] == max_proba
+        x_proba[index_array[max_proba_position]] = max_proba
         mask = x == num_categories
-    return x_proba
+    return x_proba.data.cpu()
+
+
+@torch.no_grad()
+def scan_with_mask(
+    net: nn.Module,
+    x: torch.Tensor,
+    edge_index: torch.Tensor,
+    edge_attr: torch.Tensor,
+    num_categories: int = 20,
+    output_transform: Optional[str] = None,
+) -> torch.Tensor:
+    """Generate an output for each node in the sequence by masking one node at a time."""
+    assert output_transform in [None, "proba", "logproba"]
+
+    x_ref = x
+    output_for_mask = torch.zeros_like(x_ref).to(torch.float)
+    for i in range(x_ref.size(0)):
+        x = x_ref.clone()
+        x[i] = num_categories
+        output = net(x, edge_index, edge_attr)
+        if output_transform == "proba":
+            output = torch.softmax(output, dim=1)
+        elif output_transform == "logproba":
+            output = torch.softmax(output, dim=1).log()
+        output_for_x = output.gather(1, x_ref.view(-1, 1))
+        output_for_mask[i] = output_for_x[i]
+    return output_for_mask.data.cpu()
 
 
 # === Protein design ===
@@ -179,6 +225,9 @@ def _select_residue_for_position(
             chosen_category_proba = category_probas[chosen_category]
 
         yield max_proba_index, chosen_category, chosen_category_proba
+
+
+# ASTAR approach
 
 
 @torch.no_grad()
