7f772a54ad601b86a03b367fdc346d9ff76ebf23
ostrokach <ostrokach@gmail.com>
2020-03-18 11:01:15 -0400

    Switch to msmb_theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 # serve to show the default.
 
 import os
+
+import msmb_theme
 import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
 
@@ -130,15 +132,21 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "msmb_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"collapse_navigation": False, "display_version": True}
+html_theme_options = {}  # type: ignore
+
+# Context is a dictionary passed to the templating engine.
+html_context = {"versions_json_url": "http://docs.proteinsolver.org/versions.json"}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [
+    msmb_theme.get_html_theme_path(),
+    sphinx_rtd_theme.get_html_theme_path(),
+]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
