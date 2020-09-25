ad49889de2ce7a6f2b9811456ead59d08835a322
Alexey Strokach <alex.strokach@utoronto.ca>
2020-09-25 00:48:00 -0400

    Release 0.1.23

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_md(file):
 
 setup(
     name="proteinsolver",
-    version="0.1.22",
+    version="0.1.23",
     description="Learn to solve constraint satisfaction problems (CSPs) from data.",
     long_description=read_md("README.md"),
     author="Alexey Strokach",
